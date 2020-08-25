2f8817b906b75835e03719d4b8c057a06f20b9b2
keatonb <keatonbell@utexas.edu>
2020-08-25 15:54:18 -0700

    add script and figure

---
+++ runspeedtest.py
@@ -0,0 +1,37 @@
+#!/Users/keatonb/anaconda3/bin/python
+
+import speedtest
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="whitegrid",rc={'axes.edgecolor':'.5'})
+
+servers = [1782] #Seattle
+threads = None
+
+s = speedtest.Speedtest()
+s.get_servers(servers)
+s.download(threads=threads)
+s.upload(threads=threads)
+
+results_dict = s.results.dict()
+
+f = open('/Users/keatonb/speed.dat','a')
+outline = f"{results_dict['timestamp']} {results_dict['download']*1e-6} {results_dict['upload']*1e-6} \n"
+f.write(outline)
+f.close()
+
+#plot
+df = pd.read_csv('/Users/keatonb/speed.dat',sep='\s',names=['date','download','upload'])
+df.date = pd.to_datetime(df.date).dt.tz_convert(tz = 'America/Los_Angeles')
+
+f, axs = plt.subplots(2,1,figsize=(11, 6), sharex=True)
+
+sns.lineplot(x="date", y="download", data=df, ax=axs[0])
+axs[0].set_ylabel('download speed\n(Mbps)')
+sns.lineplot(x="date", y="upload", data=df, ax=axs[1])
+axs[1].set_ylabel('upload speed\n(Mbps)')
+for ax in axs:
+    ax.set_ylim(bottom=0)
+plt.tight_layout()
+plt.savefig('/Users/keatonb/speed.pdf')
