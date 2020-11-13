b3aaad74029937b4ae30b32630f5656fb2d1060b
spencerw <scw7@uw.edu>
2020-11-13 16:49:30 -0700

    Automatically find fastest server, record ping

--- runspeedtest.py
+++ runspeedtest.py
@@ -1,38 +1,40 @@
-#!/Users/keatonb/anaconda3/bin/python
-
 import speedtest
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set(style="whitegrid",rc={'axes.edgecolor':'.5'})
 
-servers = [1782] #Seattle
 threads = None
 
 s = speedtest.Speedtest()
-s.get_servers(servers)
+server = int(s.get_best_server()['id'])
+s.get_servers([server])
 s.download(threads=threads)
 s.upload(threads=threads)
 
 results_dict = s.results.dict()
 
-datafile = '/Users/keatonb/speed.dat' #give full path
+print(results_dict['ping'])
+
+datafile = 'speed.dat' #give full path
 f = open(datafile,'a')
-outline = f"{results_dict['timestamp']} {results_dict['download']*1e-6} {results_dict['upload']*1e-6} \n"
+outline = f"{results_dict['timestamp']} {results_dict['download']*1e-6} {results_dict['upload']*1e-6} {results_dict['ping']} \n"
 f.write(outline)
 f.close()
 
 #plot
-df = pd.read_csv(datafile,sep='\s',names=['date','download','upload'])
-df.date = pd.to_datetime(df.date).dt.tz_convert(tz = 'America/Los_Angeles')
+df = pd.read_csv(datafile,sep='\s',names=['date','download','upload','ping'])
+df.date = pd.to_datetime(df.date).dt.tz_convert(tz = 'America/Phoenix')
 
-f, axs = plt.subplots(2,1,figsize=(11, 6), sharex=True)
+f, axs = plt.subplots(3,1,figsize=(16, 6), sharex=True)
 
 sns.lineplot(x="date", y="download", data=df, ax=axs[0])
 axs[0].set_ylabel('download speed\n(Mbps)')
 sns.lineplot(x="date", y="upload", data=df, ax=axs[1])
 axs[1].set_ylabel('upload speed\n(Mbps)')
+sns.lineplot(x="date", y="ping", data=df, ax=axs[2])
+axs[2].set_ylabel('ping\n(ms)')
 for ax in axs:
     ax.set_ylim(bottom=0)
 plt.tight_layout()
-plt.savefig('/Users/keatonb/speed.pdf')
+plt.savefig('speed.pdf')
